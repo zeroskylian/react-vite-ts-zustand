fb427d2ce247de6612f3b6b3e90147bd59e6c9fb
lianxinbo <lianxinbo@hengli.com>
2023-07-12 11:59:26 +0800

    优化

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,58 @@ import './App.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Container from './component/Container';
-import { Provider, sharedStore, useStore } from './store/context';
+import { Provider, sharedStore, useStore, Store } from './store/context';
+import { createStore } from 'zustand';
 
 function App() {
   return (
     <div>
       <Provider createStore={sharedStore}>
+        <div className="card">
+          <Header />
+          <Container />
+          <Footer />
+        </div>
+      </Provider>
+      <Other />
+    </div>
+  );
+}
+
+function App1() {
+  const name = 'a';
+  return (
+    <div>
+      <Provider
+        createStore={() =>
+          createStore<Store>((set, get) => ({
+            name: name,
+            count: 0,
+            increaseCount: (count: number) => {
+              set((state) => {
+                return {
+                  count: state.count + count
+                };
+              });
+            },
+            asyncIncreaseCount: async (count: number) => {
+              setTimeout(() => {
+                set((state) => {
+                  return {
+                    count: state.count + count
+                  };
+                });
+              }, 2000);
+            },
+            getNearFive: () => {
+              const count = get().count;
+              const value =
+                count % 5 < 3 ? count - (count % 5) : count + 5 - (count % 5);
+              set({ count: value });
+            }
+          }))
+        }
+      >
         <div className="card">
           <Header />
           <Container />
