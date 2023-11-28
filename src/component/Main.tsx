f8bb8f9619db542d4bca24805b1dece143f19ac8
lianxinbo <>
2023-11-28 13:43:17 +0800

    消除警告

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, createRef } from 'react';
+import React from 'react';
 
 function Main() {
   return (
@@ -12,33 +12,3 @@ function Main() {
 }
 
 export default Main;
-
-export const RefDifference: React.FC = () => {
-  const [renderIndex, setRenderIndex] = useState(1);
-  const refFromUseRef = useRef<number>();
-  const refFromCreateRef = createRef();
-  console.info(refFromUseRef.current, 'refFromUseRef.current');
-  console.info(refFromCreateRef.current, 'refFromCreateRef.current');
-  if (!refFromUseRef.current) {
-    refFromUseRef.current = renderIndex;
-  }
-
-  if (!refFromCreateRef.current) {
-    refFromCreateRef.current = renderIndex;
-  }
-  return (
-    <>
-      <p>Current render index: {renderIndex}</p>
-      <p>
-        <b>refFromUseRef</b> value: {refFromUseRef.current}
-      </p>
-      <p>
-        <b>refFromCreateRef</b> value:
-        {refFromCreateRef.current}
-      </p>
-      <button onClick={() => setRenderIndex((prev) => prev + 1)}>
-        Cause re-render
-      </button>
-    </>
-  );
-};
