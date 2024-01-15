62bd0e90e1f55f4403565e8e739bf6aac06ab7a8
lianxinbo <>
2024-01-15 16:34:00 +0800

    add debug

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Footer() {
-  return <div>Footer</div>
+export default class Footer extends Component {
+  handleButtonClick = function () {
+    console.log(this)
+  }
+
+  render() {
+    // this.handleButtonClick = this.handleButtonClick.bind(this)
+    return (
+      <div>
+        <button onClick={this.handleButtonClick}>Click me</button>
+      </div>
+    )
+  }
 }
